b518ffbabe5b90af9e9a6576cf1d579c776100ef
MarcoCasSir <wallemar4@gmail.com>
2025-11-10 17:00:41 +0100

    Añade proyecto Spring Boot en carpeta BACK

---
+++ BACK/src/main/java/com/grupotfg/weekycook/WeekyCookApplication.java
@@ -0,0 +1,13 @@
+package com.grupotfg.weekycook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekyCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekyCookApplication.class, args);
+	}
+
+}
