9a9ff2b67e5ae38d577aac3b0472730dca78d7d0
PHumanes <humanes.patricia@gmail.com>
2025-11-10 19:20:34 +0100

    Creada la entidad categoria

---
+++ BACK/src/main/java/entity/Categoria.java
@@ -0,0 +1,31 @@
+package entity;
+
+
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * ENTIDAD: Categoria
+ * Clasifica recetas (Desayuno, Italiana, Vegano, etc.)
+ * 
+ * Es una simple entidad, no tiene relaciones inversas. No necesita @ToString.Exclude
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categorias")
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String nombre; // Ej: "Italiana", "Postres" ...
+
+    private String descripcion; // Para explicar la categoría
+}
