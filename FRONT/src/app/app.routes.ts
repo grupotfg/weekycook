a2886bf309aa86c896043845a3172eb5884edfb2
PHumanes <humanes.patricia@gmail.com>
2025-12-18 13:00:05 +0100

    completo registrar con validaciones

--- FRONT/src/app/app.routes.ts
+++ FRONT/src/app/app.routes.ts
@@ -2,13 +2,26 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    //para el landing
     path: '',
     loadComponent: () => import('./features/landing/landing.component').then(m => m.LandingComponent)
   },
   {
+    //este para llevar al login
     path: 'auth/login',
     loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
   },
-  // Aquí añadiremos 'admin' y 'planner' ...  en los siguientes pasos
+{
+    //para ir a la pantalla de registro
+    path: 'auth/register',
+    loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent)
+  },
+
+
+  // Aquí añadiremos 'admin' y 'planner' en los siguientes pasos
+
+
+
+  
   { path: '**', redirectTo: '' }
 ];
