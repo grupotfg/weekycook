f4b279452af7bfbcce6ea77cf22cd1d71e93d768
MarcoCasSir <wallemar4@gmail.com>
2026-01-15 13:21:57 +0100

    en gestion usuario se evita el autocompletado del selector filtro.

--- FRONT/src/app/features/admin/components/usuarios-gestion/usuarios-gestion.component.ts
+++ FRONT/src/app/features/admin/components/usuarios-gestion/usuarios-gestion.component.ts
@@ -8,7 +8,7 @@ import { User } from '../../../../core/models/user.model';
   selector: 'app-usuarios-gestion',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './usuarios-gestion.component.html'
+  templateUrl: './usuarios-gestion.component.html',
 })
 export class UsuariosGestionComponent implements OnInit {
   private userService = inject(UserService);
@@ -28,16 +28,18 @@ export class UsuariosGestionComponent implements OnInit {
   }
 
   cargarUsuarios() {
-    this.userService.getAll().subscribe(data => this.usuarios = data);
+    this.userService.getAll().subscribe((data) => (this.usuarios = data));
   }
   //Busquedas
-get usuariosFiltrados() {
-  const filter = this.searchText.toLowerCase().trim();
-  return this.usuarios.filter(u => 
-    u.nombre.toLowerCase().includes(filter) || 
-    u.correo.toLowerCase().includes(filter)
-  );
-}
+  get usuariosFiltrados() {
+    const filter = this.searchText.toLowerCase().trim();
+    return this.usuarios.filter(
+      (u) =>
+        u.nombre.toLowerCase().includes(filter) ||
+        (u.apellido?.toLowerCase().includes(filter) ?? false) ||
+        u.correo.toLowerCase().includes(filter)
+    );
+  }
 
   nuevoUsuario() {
     this.selectedUser = this.initUser();
@@ -53,10 +55,14 @@ get usuariosFiltrados() {
 
   guardar() {
     if (this.isEditing && this.selectedUser.id) {
-      this.userService.update(this.selectedUser.id, this.selectedUser).subscribe(() => this.finalizar());
+      this.userService
+        .update(this.selectedUser.id, this.selectedUser)
+        .subscribe(() => this.finalizar());
     } else {
       // Nota: Asegúrate de que tu UserService tenga el método create()
-      this.userService.create(this.selectedUser).subscribe(() => this.finalizar());
+      this.userService
+        .create(this.selectedUser)
+        .subscribe(() => this.finalizar());
     }
   }
 
